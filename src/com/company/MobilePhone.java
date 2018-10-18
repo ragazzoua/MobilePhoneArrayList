1573e63b34659a274dde610d2bea4fca10be7c8a
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-10-18 18:42:44 +0300

    init

---
+++ src/com/company/MobilePhone.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * created by s.a.miroshnychenko 10/18/2018
+ */
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+
+        myContacts.add(contact);
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            Contact contact = this.myContacts.get(i);
+            if (contact.getName().equals(contactName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
