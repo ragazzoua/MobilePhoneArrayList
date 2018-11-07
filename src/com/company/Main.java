8d2cb645e6ec8054e29f950ea326dd5b02aec20c
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-11-07 12:31:39 +0200

    addnew Contact

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,12 +12,12 @@ public class Main {
         boolean quit = false;
         startPhone();
         printActions();
-        while (!quit){
+        while (!quit) {
             System.out.println("Enter Actions ");
-            int action= scanner.nextInt();
+            int action = scanner.nextInt();
             scanner.nextLine();
 
-            switch (action){
+            switch (action) {
                 case 0:
                     System.out.println("Shuting down");
                     quit = true;
@@ -26,25 +26,52 @@ public class Main {
                     mobilePhone.printContacts();
                     break;
 
+                case 2:
+                    addNewContact();
+                    break;
+
+                case 3:
+                    updateConatct();
+                    break;
+
+                case 4:
+                    removeContact();
+                    break;
+
+                case 5:
+                    queryConatct();
+                    break;
+
+                case 6:
+                    printActions();
+                    break;
             }
 
         }
 
     }
 
-    private static void startPhone(){
+private static void addNewContact(){
+    System.out.println("Enter new contact name");
+    String name = scanner.nextLine();
+    System.out.println("Enter new contact name");
+    String number = scanner.nextLine();
+    Contact newContact = Contact.createContact(name,number);
+}
+
+    private static void startPhone() {
         System.out.println("Starting phone");
     }
 
-    private static void printActions(){
+    private static void printActions() {
         System.out.println("\nAvailable actions:\npress");
-        System.out.println("0 - to shutdown\n"+
-                           "1 to print contacts\n"+
-                           "2 to add contacts\n"+
-                           "3 to update contacts\n"+
-                           "4 to remove contacts\n"+
-                           "5 query if an existing contact exist\n"+
-                           "6 to print list of available actions \n");
+        System.out.println("0 - to shutdown\n" +
+                "1 to print contacts\n" +
+                "2 to add contacts\n" +
+                "3 to update contacts\n" +
+                "4 to remove contacts\n" +
+                "5 query if an existing contact exist\n" +
+                "6 to print list of available actions \n");
         System.out.println("Choose your action");
     }
 }
